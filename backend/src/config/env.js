cc5127253995dbd87133ed9051cf2c450951c9bb
CheachingKov <kovcheaching20@gmail.com>
2026-06-28 21:40:11 +0700

    study-session

---
+++ backend/src/config/env.js
@@ -0,0 +1,7 @@
+import dotenv from "dotenv";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+dotenv.config({ path: join(__dirname, "../.env") });
